e088097274a3651fd7998a96349b4a5e3806e751
v_fantnzeng <v_fantnzeng@tencent.com>
2021-03-02 16:00:23 +0800

    init project

---
+++ demos/demo_1.cc
@@ -0,0 +1,8 @@
+#include "easy_alg_ae.hpp"
+
+int main(void) {
+  initgraph(640, 480);
+  _getch();
+  closegraph();
+  return 0;
+}
---
+++ include/easy_alg_ae.hpp
@@ -0,0 +1,15 @@
+//
+// Created by v_fantnzeng on 2021/3/2.
+//
+
+#ifndef EASYALGAE_EASY_ALG_AE_HPP
+#define EASYALGAE_EASY_ALG_AE_HPP
+
+#include <easyx.h>
+#include <conio.h>
+
+namespace alg_ae {
+
+}
+
+#endif // EASYALGAE_EASY_ALG_AE_HPP
